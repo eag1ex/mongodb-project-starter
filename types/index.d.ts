63be96a3870773772e16c0392d1f3cf01c76156c
eag1ex <eaglex@eaglex.net>
2021-06-22 12:05:21 +0000

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,9 @@
 declare namespace common {
-
-    type TENV = 'development' | 'production'
     declare interface IMongoConfig {
         remote?: boolean;
         database?: string;
         defaultUser?: string;
     }
-
 }
 
 declare namespace model {
@@ -37,42 +34,8 @@ declare namespace model {
         subtasks?:Array<ISubtask>
     }
 
-
-
-    // NOTE GeoPolygons wrapper
-    // declare interface ActModel {
-    //     type: string;
-    //     features: Array<CountryoPolygonModel>
-    // }
-
 }
 
-/*
- user: {
-            name: { type: String, required: true, validate: [validate.alphanumeric, 'Invalid user/name'] },
-            // password:{type:String} // this would be a salt
-        },
-
-        title: {
-            type: String,
-            required: true,
-        },
-
-        // [pending,completed]
-        status: {
-            type: String,
-            required: true,
-            validate: [validate.alpha, 'Invalid status'],
-        },
-
-        // NOTE referencing our Subtasks Model
-        subtasks: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Subtask',
-            },
-        ],
-* */
 
 //export as namespace types
 export { model }
